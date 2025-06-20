70a1c485a10ae9594534df2f0e8ca9ebec3e53f3
souravkhan12 <souravkhan654@gmail.com>
2025-06-20 11:59:33 +0530

    debounce

--- src/hook/useSearch.jsx
+++ src/hook/useSearch.jsx
@@ -1,26 +1,39 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 
 function useSearch(data, keys = []) {
   const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState(query);
   const [page, setPage] = useState(1);
   const [roles, setRoles] = useState([]);
   const [departments, setDepartments] = useState([]);
 
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedQuery(query);
+      setPage(1);
+    }, 300);
+
+    return () => clearTimeout(handler);
+  }, [query]);
+
   const { filteredData, totalPages } = useMemo(() => {
     let result = data;
 
-    if (query) {
+    if (debouncedQuery) {
       result = data.filter((item) =>
         keys.some((key) =>
-          item[key].toLowerCase().includes(query.toLowerCase())
+          item[key].toLowerCase().includes(debouncedQuery.toLowerCase())
         )
       );
     }
 
+    // filter by status
     if (roles.length > 0) {
       const q = JSON.stringify(roles);
       result = result.filter((item) => q.includes(item.status));
     }
+
+    // filter by department
     if (departments.length > 0) {
       const q = JSON.stringify(departments);
       result = result.filter((item) => q.includes(item.department));
@@ -30,7 +43,7 @@ function useSearch(data, keys = []) {
     const paginatedData = result.slice((page - 1) * 5, page * 5);
 
     return { filteredData: paginatedData, totalPages };
-  }, [data, query, keys, page, roles]);
+  }, [data, debouncedQuery, keys, page, roles, departments]);
 
   return {
     query,
